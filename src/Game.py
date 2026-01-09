98ace136e1c2f0b5bed1620d1efc42ba629b659b
lmerat-iut90 <leslie.merat@edu.univ-fcomte.fr>
2026-01-09 16:04:25 +0100

    add function append + update function isValidCoup

--- src/Game.py
+++ src/Game.py
@@ -1,3 +1,4 @@
+from Grille import Grille
 class Game:
     def __init__(self, grille):
         self.grille = grille
@@ -6,18 +7,37 @@ class Game:
         self.finished = False
 
 # faire une fonction append() pour ajouter le coup dans la grille (utilise la fonction isValidCoup)
+    def append(self, valeur, x, y):
+        if self.isValidCoup(valeur):
+            self.grille[x][y] = valeur
 
 # faire une fonction pour récupérer les coordonnées (x,y) du coup
+    def getXCoup(self, coup):
+        for i in range(1, self.grille.len()):
+            if self.grille[i] == coup:
+                    return i
+
+    def getYCoup(self, coup):
+        for i in range(1, self.grille.len()):
+            for j in range(1, self.grille[i].len()):
+                if self.grille[i][j] == coup:
+                    return j
 
 # faire une fonction pour récupérer le chiffre à une coordonnée donnée (peut être pas utile)
 
+# Vérifie si le coup est valide
     def isValidCoup(self, coup):
         for i in range(9):
             if coup in self.grille[i]:
                 return False
             for j in range(9):
-                if coup in self.grille[0][j]: # remplacer le 0 par la coordonnée du coup
+                if coup in self.grille[self.getXCoup(coup)][j]:
                     return False
+        blocs = []
+        for bi in range(0, 9, 3):
+            for bj in range(0, 9, 3):
+                bloc = [self.grille[i][bj:bj + 3] for i in range(bi, bi + 3)]
+                blocs.append(bloc)
         # pour vérifier pour le carré -> prendre les coordonnées du coup puis ajouté 3 au x et 3 au y pour vérifier les nombre autour de lui
         return True
 
